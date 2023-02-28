41ddc146fecf654441b3ad7e77d45fe590341975
Adam Gough <mm22ajg@leeds.ac.uk>
2023-02-28 15:13:32 +0000

    Created a function for the probability of y1

--- R/Evaluate design function.R
+++ R/Evaluate design function.R
@@ -1,3 +1,10 @@
+prob_y1 <- function(y1, n1){
+  # Calculates the probability of observing y_1 responses in n_1 trials
+  # under a Beta-binomial model with Beta(a = 0.5, b = 0.5) prior.
+  
+  choose(n1, y1) * beta(y1 + 0.5, n1 - y1 + 0.5) / beta(0.5, 0.5)
+}
+
 evaluate_design <- function(lambda, gamma, n1, n2, theta) {
   
   # Estimate the type I error rate, type II error rate, and the expected 
